68bf1f15f1758bce6f29e32dffecb6cf66f3fd42
Robert Cabrera <rocabrer@pichincha.com>
2024-01-05 11:14:41 -0500

    initial commit - create project and folder structure

---
+++ src/main/java/com/pichincha/evaluation/fdamsaevaluationjsonprocessor/FdaMsaEvaluationJsonProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.pichincha.evaluation.fdamsaevaluationjsonprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdaMsaEvaluationJsonProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FdaMsaEvaluationJsonProcessorApplication.class, args);
+  }
+}
---
+++ src/test/java/com/pichincha/evaluation/fdamsaevaluationjsonprocessor/FdaMsaEvaluationJsonProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.evaluation.fdamsaevaluationjsonprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdaMsaEvaluationJsonProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
